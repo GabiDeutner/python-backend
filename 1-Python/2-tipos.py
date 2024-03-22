14d312259baf39e4e2546fa6018d840d9606bd4a
Gabriela Paulini Deutner <deutnerg@gmail.com>
2024-03-22 17:43:38 -0300

    iniciando estudos py

---
+++ 1-Python/2-tipos.py
@@ -0,0 +1,14 @@
+name = 'Resident Evil 4 Remake'
+yearLaunch = 2023
+gamePrice = 300.00
+planIncluded = False
+
+print(name)
+print(yearLaunch)
+print(gamePrice)
+print(planIncluded)
+
+print(type(name))
+print(type(yearLaunch))
+print(type(gamePrice))
+print(type(planIncluded))
---
+++ 1-Python/3-input.py
@@ -0,0 +1,8 @@
+name = input("Digite o nome do jogo \n")
+yearLaunch = int(input("Digite o ano de lançamento\n"))
+gamePrice = float(input("Digite o preço do jogo\n"))
+noteRating= float(input("Digite a nota de avaliação do jogo\n"))
+print(name)
+print(yearLaunch)
+print(gamePrice)
+print(noteRating)
