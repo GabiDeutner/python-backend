b5b63567feda6c382664e7e891f59ffc26df1436
Gabriela Paulini Deutner <deutnerg@gmail.com>
2024-04-09 15:11:55 -0300

    estudando concatenacao

---
+++ Treinando/4.2-concatena-treino.py
@@ -0,0 +1,7 @@
+name = input("Digite seu nome: \n")
+idade = int(input("Digite sua idade: \n"))
+profissao = input("Digite sua profissão: \n")
+altura = float(input("Digite sua altura: \n"))
+print("### Dados da Gabs ####")
+print("====================")
+print("Nome: ", name, "\nIdade: ", idade, "\nProfissão: ", profissao, "\nAltura: ", altura)
---
+++ Treinando/4.3-concatena-treino.py
@@ -0,0 +1,8 @@
+name = input("Digite seu nome: \n")
+idade = int(input("Digite sua idade: \n"))
+profissao = input("Digite sua profissão: \n")
+altura = float(input("Digite sua altura: \n"))
+print("### Dados da Gabs ####")
+print("====================")
+
+print(f"Nome: {name} \nIdade: {idade} \nProfissão: {profissao} \nAltura: {altura}")
