3ee1e8675bb3dcc9ee7dec7eaf3030c9c19881a1
Gabriela Paulini Deutner <deutnerg@gmail.com>
2024-04-09 21:56:33 -0300

    listas

---
+++ 1-Python/10-listas.py
@@ -0,0 +1,17 @@
+gameData = ["Fifa23", 2022, 90.50, 8.5]
+print(gameData)
+
+gamesList = ["Fifa23", "Star Wars", "The Legend of Zelda", "Red Dead 2"]
+print(gamesList)
+
+# 1 - Busque os dois primeiros itens da lista
+print(gamesList[0:2])
+
+# 2 - Busque o último item da lista
+print(gamesList[-1])
+
+# 3 - Busca jogos até uma posição
+print(gamesList[:2])
+
+# 4 - Busca jogos de uma posição em diante
+print(gamesList[2:])
