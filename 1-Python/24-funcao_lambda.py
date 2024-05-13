500c65c74606a461136f65a5dd970917b839a728
Gabi Deutner <45831598+GabiDeutner@users.noreply.github.com>
2024-05-13 17:00:01 -0300

    Create 24-funcao_lambda.py

---
+++ 1-Python/24-funcao_lambda.py
@@ -0,0 +1,13 @@
+power = lambda num: num ** 2
+pair = lambda x: x%2==0
+divNum = lambda x,y : x/y
+reverse = lambda s: s[::-1]
+
+print(power(5))
+print(power(9))
+print(pair(27))
+print(pair(30))
+print(divNum(10,2))
+print(divNum(7,2))
+print(reverse("Função"))
+print(reverse("Tecnologia"))
