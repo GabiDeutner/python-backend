2500046b767afc961ad7aabc5d1b525d4be01059
Gabi Deutner <45831598+GabiDeutner@users.noreply.github.com>
2024-05-13 11:47:32 -0300

    Update exercicio04.py
    adicionando comentarios

--- exercicios/exercicio04.py
+++ exercicios/exercicio04.py
@@ -4,12 +4,21 @@
 Faça um programa para escrever a contagem regressiva do lançamento de um foguete. O programa deve imprimir 10, 9, 8, …, 1, 0 e disparar um “beep”.
 '''
 
+#resolução 1 - sem o beep
+
+x = 10
+while x >= 0:
+    print(x)
+    x -= 1
+
+
+# resolução 2 - com o beep
 import winsound
 x = 10
 while x >= 0:
     print(x)
     x = x - 1
-winsound.Beep(2500, 500)
+winsound.Beep(2500, 500) # frequencia, duração do beep
 
 
 '''
