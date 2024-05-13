909e8c78516b072fa023284a7752094611d3d1c1
Gabi Deutner <45831598+GabiDeutner@users.noreply.github.com>
2024-05-13 11:27:42 -0300

    Create exercicio04.py
    resolução exercício 4 de laços de repetição

---
+++ exercicios/exercicio04.py
@@ -0,0 +1,27 @@
+'''
+## Contagem Regressiva
+
+Faça um programa para escrever a contagem regressiva do lançamento de um foguete. O programa deve imprimir 10, 9, 8, …, 1, 0 e disparar um “beep”.
+'''
+
+import winsound
+x = 10
+while x >= 0:
+    print(x)
+    x = x - 1
+winsound.Beep(2500, 500)
+
+
+'''
+## Tabuada
+
+Faça um programa que calcule a tabuada de um número, com valores iniciais e finais informados pelo usuário
+'''
+
+number = int(input("Tabuada de: "))
+begin = int(input("De: "))
+end = int(input("Até: "))
+x = begin
+while x <= end:
+    print(f"{number} x {x} = {number * x}")
+    x = x + 1
