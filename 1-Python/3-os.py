b81bd5d2a9648a91784040befe4ec2251cd29947
Gabi Deutner <45831598+GabiDeutner@users.noreply.github.com>
2024-05-14 17:43:49 -0300

    Create 3-os.py

---
+++ 1-Python/3-os.py
@@ -0,0 +1,16 @@
+import os
+
+# Pasta atual
+print(os.getcwd())
+
+# Lista arquivos e pastas
+print(os.listdir())    
+
+# Versão do SO
+os.system('ver')
+
+# Configurações da Máquina
+os.system('systeminfo')
+
+# Limpar a tela
+os.system('cls')
