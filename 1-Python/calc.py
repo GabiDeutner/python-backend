f0d1e8656f55bd0424e36ef1563bd6f645a61d77
Gabi Deutner <45831598+GabiDeutner@users.noreply.github.com>
2024-05-14 16:22:42 -0300

    Create calc.py

---
+++ 1-Python/calc.py
@@ -0,0 +1,15 @@
+def sum(a, b):
+    result = a + b
+    return result
+
+def subtraction(a, b):
+    result = a - b
+    return result
+
+def multiplication(a, b):
+    result = a * b
+    return result
+
+def division(a, b):
+    result = a / b
+    return result
